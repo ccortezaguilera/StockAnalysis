c6101d25985f79e2dc09793cc68ab3272ffb07fa
ccortezaguilera <ccortezaguilera@dons.usfca.edu>
2017-06-12 21:03:38 -0700

    fix extract_data_day

--- stock_day.py
+++ stock_day.py
@@ -1,16 +1,17 @@
 # stock_day.py - class definition
-
+import datetime
+from datetime import timedelta
 
 class StockDay(object):
 
-    def __init__(self, date, open, high, low, close, volume, adj_close):
+    def __init__(self, date, open, high, low, close, volume):
         self.date = date
         self.open = open
         self.high = high
         self.low = low
         self.close = close
         self.volume = volume
-        self.adj_close = adj_close
+        #self.adj_close = adj_close
 
     def get_abs_diff(self):
         return abs(self.close - self.open)
@@ -27,6 +28,13 @@ class StockDay(object):
     def percent_decrease(self):
         return ((self.open - self.close) / self.open) * 100
 
+    def __str__(self):
+        return self.date + ' ' + self.open + ' ' + self.high + ' ' + ' ' + self.low + ' ' + self.close + ' ' + self.volume
+
+
+def datetime_to_str(date):
+    return  date.strftime('%m/%d/%Y')
+
 import os, os.path
 
 
@@ -41,7 +49,7 @@ def __safe_open_w(path, file):
         except OSError as e:
             print(e.errno, e.winerror)
             raise
-    return open(os.path.join(path,file), 'w+')
+    return open(os.path.join(path,file), 'wb+')
 
 '''
 extract_day_data
@@ -49,28 +57,32 @@ extract_day_data
 @:parameter day - the day currently extracting from 0 to N
 @:days - the unix time (epoch time) the function has already extracted and has kept for memory
 '''
-def extract_day_data(day_data, day=0, days={}):
+def extract_day_data(day_data, start=0, data={}):
     #todo decide on the data structure to hold the days we might want our stock_history
-    import datetime
-    if (day_data[day][0] == 'a'):
+    if (day_data[0][0] == 'a'):
         # getting the month day and year -> mm/dd/year
-        indicator_day = datetime.datetime.fromtimestamp(int(day_data[0][1:])).strftime('%m/%d/%Y')
-
-        if len(days) > 0:
-            days.pop()
-        days.append(indicator_day)
+        day = datetime.datetime.fromtimestamp(int(day_data[0][1:]))
+        day_str = datetime_to_str(day)
+        data[day_str] = StockDay(day,float(day_data[1]), float(day_data[2]), float(day_data[3]), float(day_data[4]), int(day_data[5]))
+        return day
     else:
-        days[-1]
+        new_day = start + timedelta(days=int(day_data[0]))
+        new_day_str = datetime_to_str(new_day)
+        data[new_day_str] = StockDay(new_day,float(day_data[1]), float(day_data[2]), float(day_data[3]), float(day_data[4]), int(day_data[5]))
+        return start
+
 
 def process_data(data):
     row_count = len(data)
     print(row_count)
-    #print(data)
+    start = 0
+    collection = {}
     for line in data:
         stock_day_data = line.split(',')
-        extract_day_data(stock_day_data)
+        if (stock_day_data[0][0:8] != "TIMEZONE"):
+            start = extract_day_data(stock_day_data, start)
 
-
+    return collection
 
 def main():
     from urllib.request import urlopen
@@ -85,13 +97,14 @@ def main():
     url = 'https://www.google.com/finance/getprices?q=' + symbol + '&x=' + 'NASDAQ' + '&i=86400&p=40Y&f=d,c,h,l,o,v'
     with urlopen(url) as response:
         body = response.read()
+        data = body.decode('utf-8')
         body_str = body.decode('utf-8').split('\n')
         #TODO process data
-        process_data(body_str[7:len(body_str)-1])
+        collection = process_data(body_str[7:len(body_str)-1])
         #filename = symbol + '.csv'
         #with __safe_open_w('stock', filename) as f:
         #    print('writing ' + f.name)
-        #    f.write(data)
+        #    f.write(body)
         #    f.close()
 
 if __name__ == '__main__':
